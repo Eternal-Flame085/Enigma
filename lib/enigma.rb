446107d87d9583220a9b7a973c14b09e33ac86bb
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-19 14:15:38 -0400

    made key_combinatiob helper methiod

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,4 +3,18 @@ class Enigma
   def initialize
     @alphabet = ("a".."z").to_a << " "
   end
+
+  def encrypt(encryption, key, date)
+    keys = key_combinations(key)
+
+  end
+
+  def key_combinations(key)
+    collector = []
+    key.split("").each_cons(2) {|consecutive| collector << consecutive}
+    collector.each_with_index do |keys, index|
+      collector[index] = keys[0]+keys[1]
+    end
+    collector
+  end
 end
