1b573986c2de0b77e62c8440204a62f91cf4ac6b
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-19 17:28:55 -0400

    made test for encrypt with key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -42,4 +42,17 @@ class EnigmaTest < Minitest::Test
 
 		assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
 	end
+
+	def test_encrypt_with_key
+		enigma = Enigma.new
+
+		require "pry"; binding.pry
+		expected = {
+			encryption: "keder ohulw",
+			key: "02715",
+			date: " "
+		}
+
+		assert_equal expected, enigma.encrypt("hello world", "02715")
+	end
 end
