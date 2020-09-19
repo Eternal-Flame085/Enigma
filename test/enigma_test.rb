b92d5470ed8f3c021bf69592b0810aeb5560576e
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-19 13:43:36 -0400

    test seeing enigma has alphabet array

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,4 +8,14 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
 	end
+
+	def test_it_has_alphabet
+		enigma = Enigma.new
+
+		expected = ["a", "b", "c", "d", "e", "f", "g",
+								"h", "i", "j", "k", "l", "m", "n",
+								"o", "p", "q", "r", "s", "t", "u",
+								"v", "w", "x", "y", "z", " "]
+		assert_equal expected, enigma.alphabet
+	end
 end
