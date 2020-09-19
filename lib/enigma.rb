91d6c1194a433a03a31f033a96d9dc7aafbe8b22
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-19 16:19:10 -0400

    Got the encryption to work

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,10 +4,23 @@ class Enigma
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt(encryption, key, date)
+  def encrypt(message, key, date)
     keys = key_combinations(key)
     offsets = date_offset(date)
-
+    encryption = ""
+    key_offset_counter = 0
+    message.each_char do |letter|
+      shift = keys[key_offset_counter].to_i + offsets[key_offset_counter].to_i
+      alphabet_tracker = @alphabet.index(letter.downcase)
+      shift.times do
+        alphabet_tracker = alphabet_tracker + 1
+        alphabet_tracker = 0 if alphabet_tracker == 27
+      end
+      encryption += @alphabet[alphabet_tracker]
+      key_offset_counter += 1
+      key_offset_counter = 0 if key_offset_counter == 4
+    end
+    require "pry"; binding.pry
   end
 
   def key_combinations(key)
