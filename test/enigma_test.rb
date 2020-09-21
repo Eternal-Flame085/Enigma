eaa8b5438f2b3e7734f52bd5405830a8b2e646ff
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-21 13:45:51 -0400

    addded tests to reflect refactor

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -128,4 +128,16 @@ class EnigmaTest < Minitest::Test
 		assert_equal 10, enigma.alphabet_index_finder(3, "h", "encrypt")
 		assert_equal 7, enigma.alphabet_index_finder(3, "k", "decrypt")
 	end
+
+	def test_encrypt_shift
+		enigma = Enigma.new
+
+		assert_equal 10, enigma.encrypt_shift(3, "h")
+	end
+
+	def test_decrypt_shift
+		enigma = Enigma.new
+
+		assert_equal 7, enigma.decrypt_shift(3, "k")
+	end
 end
